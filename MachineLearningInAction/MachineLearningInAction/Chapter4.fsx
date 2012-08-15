e51fb6104e3e5b09cc628c857b256f2c80cd90c1
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2012-08-15 16:07:41 -0700

    NaiveBayes cleanup complete
    Extracted out relevant methods from Chapter4.fsx script to NaiveBayes
    module, and cleaned up the code.

--- MachineLearningInAction/MachineLearningInAction/Chapter4.fsx
+++ MachineLearningInAction/MachineLearningInAction/Chapter4.fsx
@@ -12,35 +12,24 @@ let dataset =
        ("Ham",  "Mr Licks ate my steak how to stop him");
        ("Spam", "Quit buying worthless dog food stupid") |]
 
-let estimate dataset words =
-    let init = words |> Seq.map (fun w -> (w, 1.0))
+// Retrieve all words from the dataset
+let tokens = extractWords dataset
+
+// Create 2 classifiers, using all the words found
+let setClassifier = classifier setOfWords dataset tokens
+let bagClassifier = classifier bagOfWords dataset tokens
+
+// apply the set-of-words classifier 
+// to all elements from the dataset,
+// and retrieves actual and predicted labels
+let setOfWordsTest =
     dataset
-    |> Seq.fold (fun state (label, sample) -> bagFold state sample) init
+    |> Seq.map (fun t -> fst t, setClassifier (snd t))
+    |> Seq.toList
 
-let evaluate dataset words =
-    let size = Seq.length dataset
+// apply the bag-of-words classifier 
+// to all elements from the dataset.
+let bagOfWordsTest =
     dataset
-    |> prepare
-    |> Seq.groupBy fst
-    |> Seq.map (fun (label, data) -> label, Seq.length data, estimate data words)
-    |> Seq.map (fun (label, total, tokenCount) ->
-        let tokensTotal = Seq.sumBy (fun t -> snd t) tokenCount
-        label, (float)total/(float)size, Seq.map (fun (token, count) -> token, count / tokensTotal) tokenCount)
-
-let classify dataset words text =
-    let estimator = evaluate dataset words
-    let tokenized = vocabulary text
-    estimator
-    |> Seq.map (fun (label, proba, tokens) ->
-        label,
-        tokens
-        |> Seq.fold (fun p token -> 
-            if Seq.exists (fun w -> w = fst token) tokenized 
-            then p + log(snd token) 
-            else p) (log proba))
-    |> Seq.maxBy snd
-    |> fst
-
-let testWords = extractWords dataset
-let classifier = classify dataset testWords
-let test = Seq.map (fun s -> snd s) dataset |> Seq.map (fun t -> classifier t) |> Seq.toList
+    |> Seq.map (fun t -> fst t, bagClassifier (snd t))
+    |> Seq.toList
--- MachineLearningInAction/MachineLearningInAction/NaiveBayes.fs
+++ MachineLearningInAction/MachineLearningInAction/NaiveBayes.fs
@@ -14,7 +14,8 @@ module NaiveBayes =
         matchWords.Matches(text)
         |> Seq.cast<Match>
         |> Seq.groupBy (fun m -> m.Value)
-        |> Seq.map (fun (value, groups) -> value.ToLower(), (groups |> Seq.length))
+        |> Seq.map (fun (value, groups) -> 
+            value.ToLower(), (groups |> Seq.length))
 
     // Extracts all words used in a string.
     let vocabulary text =
@@ -47,7 +48,7 @@ module NaiveBayes =
         state
         |> Seq.map (fun (token, count) -> 
             if Seq.exists (fun (t, c) -> t = token) sample 
-            then (token, count + 1.0) 
+            then (token, count + 1) 
             else (token, count))
 
     // Bag-of-Words Accumulator function: 
@@ -59,5 +60,62 @@ module NaiveBayes =
         state
         |> Seq.map (fun (token, count) -> 
             match Seq.tryFind (fun (t, c) -> t = token) sample with
-            | Some((t, c)) -> (token, count + (float)c) 
+            | Some((t, c)) -> (token, count + c) 
             | None ->         (token, count))
+
+    // Aggregate words frequency across the dataset,
+    // using the provided folder.
+    // (Supports setFold and bagFold)
+    let frequency folder dataset words =
+        let init = words |> Seq.map (fun w -> (w, 1))
+        dataset
+        |> Seq.fold (fun state (label, sample) -> folder state sample) init
+
+    // Convenience functions for training the classifier
+    // using set-of-Words and bag-of-Words frequency.
+    let bagOfWords dataset words = frequency bagFold dataset words
+    let setOfWords dataset words = frequency setFold dataset words
+
+    // Converts 2 integers into a proportion.
+    let prop (count, total) = (float)count / (float)total
+
+    // Train based on a set of words and a dataset:
+    // the dataset is "tokenized", and broken down into
+    // one dataset per classification label.
+    // For each group, we compute:
+    // the proportion of the group relative to total,
+    // the probability of each word within the group.
+    let train frequency dataset words =
+        let size = Seq.length dataset
+        dataset
+        |> prepare
+        |> Seq.groupBy fst
+        |> Seq.map (fun (label, data) -> 
+            label, Seq.length data, frequency data words)
+        |> Seq.map (fun (label, total, tokenCount) ->
+            let totTokens = Seq.sumBy (fun t -> snd t) tokenCount
+            label, prop(total, size), Seq.map (fun (token, count) -> token, prop(count, totTokens)) tokenCount)
+
+    // Classifier function:
+    // the classifier is trained on the dataset,
+    // using the words and frequency folder supplied.
+    // A piece of text is classified by computing
+    // the "likelihood" it belongs to each possible label,
+    // by checking the presence and weight of each
+    // "classification word" in the tokenized text,
+    // and returning the highest scoring label.
+    // Probabilities are log-transformed to avoid underflow.
+    // See "Chapter4.fsx" for an illustration.
+    let classifier frequency dataset words text =
+        let estimator = train frequency dataset words
+        let tokenized = vocabulary text
+        estimator
+        |> Seq.map (fun (label, proba, tokens) ->
+            label,
+            tokens
+            |> Seq.fold (fun p token -> 
+                if Seq.exists (fun w -> w = fst token) tokenized 
+                then p + log(snd token) 
+                else p) (log proba))
+        |> Seq.maxBy snd
+        |> fst
