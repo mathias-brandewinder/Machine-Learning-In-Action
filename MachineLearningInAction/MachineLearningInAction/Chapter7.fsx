613db6a10500644a85d9c72a328392918680003f
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2012-12-28 00:03:31 -0800

    AdaBoost: script in progress

--- MachineLearningInAction/MachineLearningInAction/Chapter7.fsx
+++ MachineLearningInAction/MachineLearningInAction/Chapter7.fsx
@@ -8,16 +8,16 @@ let dataset, classLabels =
        [| 2.0; 1.0 |] |], 
     [| 1.0; 1.0; -1.0; -1.0; 1.0 |]
 
-let stumpClassify (observation: float []) dimension threshold =
+let stumpClassify dimension threshold (observation: float []) =
     if observation.[dimension] >= threshold
     then 1.0
     else -1.0
 
-let weightedError (observation: float[]) label weight dimension threshold =
-    let prediction = stumpClassify observation dimension threshold
+let weightedError (observation: float[]) label weight classifier =
+    let prediction = classifier observation
     if prediction = label then 0.0 else weight
 
-let buildStump (dataset:float[][]) classLabels weights  =
+let buildStump (dataset:float[][]) classLabels weights =
     seq {
         let numSteps = 10.0
         let dimensions = dataset.[0].Length
@@ -26,13 +26,48 @@ let buildStump (dataset:float[][]) classLabels weights  =
             let min, max = Array.min column, Array.max column
             let stepSize = (max - min) / numSteps
             for threshold in min .. stepSize .. max do
+                let stump = stumpClassify dim threshold
                 let error =
                     Array.zip3 dataset classLabels weights
                     |> Array.map (fun (obs, lbl, w) -> 
-                        weightedError obs lbl w dim threshold)
+                        weightedError obs lbl w stump)
                     |> Array.sum
                 yield dim, threshold, error }
     |> Seq.minBy (fun (dim, thresh, err) -> err)
 
 let weights = [| 0.2; 0.2; 0.2; 0.2; 0.2 |]
 buildStump dataset classLabels weights
+
+type WeakLearner = { Alpha: float; Classifier: float [] -> float }
+
+let normalize (weights: float []) = 
+    let total = weights |> Array.sum 
+    Array.map (fun w -> w / total) weights
+
+let train dataset labels =
+
+    let rec update iter stumps w =
+
+        let dim, thresh, err = buildStump dataset labels w
+        printfn "Error: %f" err
+        let stump = stumpClassify dim thresh
+        let alpha = 0.5 * log ((1.0 - err) / err)
+        let learner = { Alpha = alpha; Classifier = stump }
+
+        let w' = 
+            Array.zip3 dataset labels w
+            |> Array.map (fun (obs, lbl, w) -> 
+                match stump obs = lbl with
+                | true  -> w * exp (-alpha)
+                | false -> w * exp alpha)
+            |> normalize
+        let stumps' = learner :: stumps
+
+        match iter > 0 with
+        | false -> stumps'
+        | true  -> update (iter - 1) stumps' w'
+
+    let size = Array.length dataset
+    let weights = [| for i in 1 .. size -> 1.0 / (float)size |]
+
+    update 10 [] weights
